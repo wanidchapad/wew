8ae4466c6180413651aeb07f55f73d6f71e284af
wanidchapad <diw123524@hotmail.com>
2017-04-10 11:27:28 +0700

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,16 @@
+package a2;
+
+public class a2 {
+
+  public static void main(String[]args){
+	  int sum=0;
+	  int i=1;
+	  for(i=1;i<=100;i++){
+		  sum+=i;
+	  }
+System.out.println("Summetion 1 to 100="+sum);
+
+
+	}
+
+}
